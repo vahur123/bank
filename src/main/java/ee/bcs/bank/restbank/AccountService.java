e4aeabd48f55c349b6d2f5d494ace7aba0a50a5b
Aleksei Gornev <vigadeparandus@gmail.com>
2022-01-27 15:40:17 +0200

    TASK-3 added updateOwnerDetails

--- src/main/java/ee/bcs/bank/restbank/AccountService.java
+++ src/main/java/ee/bcs/bank/restbank/AccountService.java
@@ -71,4 +71,23 @@ public class AccountService {
         }
         return null;
     }
+
+    public RequestResult updateOwnerDetails(List<AccountDto> accounts, AccountDto accountDto) {
+        RequestResult requestResult = new RequestResult();
+
+        int accountId = accountDto.getId();
+        if(!accountIdExist(accounts, accountId)) {
+            requestResult.setError("Account ID" + accountId + "does not exist!");
+            return requestResult;
+        }
+
+        AccountDto account = getAccountById(accounts, accountId);
+        account.setFirstName(accountDto.getFirstName());
+        account.setLastName(accountDto.getLastName());
+
+        requestResult.setAccountId(accountId);
+        requestResult.setMessage("Sucsessfully updated account.");
+        return requestResult;
+
+    }
 }
