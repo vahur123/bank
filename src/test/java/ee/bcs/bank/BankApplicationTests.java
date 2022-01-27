f102113e9a2eb31bce3bc293e1a2e5c4c7303b1e
Õpilane <opilane@ak.sise>
2022-01-27 09:57:29 +0200

    Initial commit

---
+++ src/test/java/ee/bcs/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
